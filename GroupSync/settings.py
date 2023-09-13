fa73a815972e349bd80570a32026fcf7952e487b
singh123456 <79777067+singh123456@users.noreply.github.com>
2023-09-13 08:57:00 +0530

    Update settings.py

--- GroupSync/settings.py
+++ GroupSync/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/images/'
-STATIC_ROOT = '/home/GroupSync/aakash123.pythonanywhere.com/static/styles'
+STATIC_ROOT = '/home/GroupSync/aakash123.pythonanywhere.com/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
